1a8f64acc1fdfe515fcf2a1d97b768e9625a87bc
Jiayu Huang <jiayu.huang@ufl.edu>
2022-10-26 13:53:08 -0400

    alluvial

--- clustering.R
+++ clustering.R
@@ -45,6 +45,10 @@ fviz_nbclust(scaledata, kmeans, method = "wss", k.max=10)
 # Perform k means when k=2
 set.seed(1)
 km <- kmeans(scaledata, centers = 2, nstart = 25)
+km1 <- kmeans(scaledata1, centers = 2, nstart = 25)
+km2 <- kmeans(scaledata2, centers = 2, nstart = 25)
+km3 <- kmeans(scaledata3, centers = 2, nstart = 25)
+km4 <- kmeans(scaledata4, centers = 2, nstart = 25)
 #plot results of final k-means model
 fviz_cluster(km, data = scaledata)
 
@@ -54,28 +58,60 @@ cat_5k <- do.call(rbind, Map(data.frame, A=row, B=cat_5k))
 rownames(cat_5k) <- 1:nrow(cat_5k)
 colnames(cat_5k) <- c('Sample', 'Cluster')
 
+cat_10k <- km1[[1]]
+row <- rownames(scaledata1)
+cat_10k <- do.call(rbind, Map(data.frame, A=row, B=cat_10k))
+rownames(cat_10k) <- 1:nrow(cat_10k)
+colnames(cat_10k) <- c('Sample', 'Cluster')
+
+cat_1k <- km2[[1]]
+row <- rownames(scaledata2)
+cat_1k <- do.call(rbind, Map(data.frame, A=row, B=cat_1k))
+rownames(cat_1k) <- 1:nrow(cat_1k)
+colnames(cat_1k) <- c('Sample', 'Cluster')
+
+cat_100 <- km3[[1]]
+row <- rownames(scaledata3)
+cat_100 <- do.call(rbind, Map(data.frame, A=row, B=cat_100))
+rownames(cat_100) <- 1:nrow(cat_100)
+colnames(cat_100) <- c('Sample', 'Cluster')
+
+cat_10 <- km4[[1]]
+row <- rownames(scaledata4)
+cat_10 <- do.call(rbind, Map(data.frame, A=row, B=cat_10))
+rownames(cat_10) <- 1:nrow(cat_5k)
+colnames(cat_10) <- c('Sample', 'Cluster')
+
+mydf2 <- read.csv(file = 'f_data.csv')
+anno <- mydf2[-1]
+anno$Cluster <- cat_5k$Cluster
+anno1 <- mydf2[-1]
+anno1$Cluster <- cat_10k$Cluster
+anno2 <- mydf2[-1]
+anno2$Cluster <- cat_1k$Cluster
+anno3 <- mydf2[-1]
+anno3$Cluster <- cat_100$Cluster
+anno4 <- mydf2[-1]
+anno4$Cluster <- cat_10$Cluster
+
 # alluvial plot
 install.packages("ggalluvial")
 library(ggplot2)
 library(ggalluvial)
 
-ggplot(data = cat_5k,
-       aes(axis1 = Sample, axis2 = Cluster, y = 1)) + 
-  scale_x_discrete(limits = c("Sample", 'Cluster')) + 
-  geom_alluvium(aes(fill = Cluster), show.legend = FALSE) + 
-  geom_stratum() + aes(label = after_stat(stratum)) + 
-  theme_void() + ggtitle("5000 genes clusters") + 
-  theme(plot.title = element_text(hjust = 0.5, size = 20))
+ggplot(data = anno4,
+       aes(axis1 = Symptoms, axis2 = Cluster)) + 
+  geom_alluvium(aes(fill = Cluster)) + 
+  geom_stratum() + 
+  geom_text(stat = "stratum",
+            aes(label = after_stat(stratum))) + 
+  theme_void()
 
 
 # Heatmap
 install.packages('pheatmap')
 library(pheatmap)
-mydf2 <- read.csv(file = 'f_data.csv')
-anno <- mydf2[-1]
 row.names(anno) <- mydf2$PatientID
-anno$Cluster <- cat_5k$Cluster
-
 
 pheatmap(scaledata,
          cluster_cols = T, cluster_rows = T, scale = "none",
